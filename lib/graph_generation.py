3b4a77487d52e4530a11c2f7c68794c5e541eb47
trouleau <william.trouleau@epfl.ch>
2019-09-30 13:35:51 +0200

    Fixed graph generation random seed

--- lib/graph_generation.py
+++ lib/graph_generation.py
@@ -30,7 +30,7 @@ def make_ebola_network(n_nodes, p_in, p_out, seed=None):
         base_graph_data = json.load(f)
     base_graph = nx.readwrite.json_graph.node_link_graph(base_graph_data)
     cluster_names = list(base_graph.nodes())
-    cluster_sizes = [int(np.round(n_nodes * base_graph.node[u]['size']))
+    cluster_sizes = [int(np.ceil(n_nodes * base_graph.node[u]['size']))
                      for u in cluster_names]
     node_names = np.repeat(cluster_names, cluster_sizes)
     n_nodes = sum(cluster_sizes)
@@ -39,14 +39,13 @@ def make_ebola_network(n_nodes, p_in, p_out, seed=None):
     base_adj[base_adj == 1] = p_out
     base_adj[np.eye(len(base_graph.nodes()), dtype=bool)] = p_in
     # Generate stoch block model graph
-    graph = nx.generators.stochastic_block_model(cluster_sizes, base_adj, seed)
+    graph = nx.generators.stochastic_block_model(cluster_sizes, base_adj, seed=seed)
     # Assign district attribute to each node
     for u, district in zip(graph.nodes(), node_names):
         graph.node[u]['district'] = district
     # Sanity check for name assignment of each cluster
     num_unique_block_district = len(set([(node_data['block'], node_data['district']) for u, node_data in graph.nodes(data=True)]))
     assert num_unique_block_district == len(cluster_names)
-
     # Extract the giant component
     graph = max(nx.connected_component_subgraphs(graph), key=len)
     return graph
