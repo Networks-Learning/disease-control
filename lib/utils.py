b54cc0222a8e1dce9e477e6e5e80f1703bd00ebf
trouleau <william.trouleau@epfl.ch>
2019-10-02 14:42:00 +0200

    Fixed utility function to work with minimal output data + added a function to compute R0 per country

--- lib/utils.py
+++ lib/utils.py
@@ -1,20 +1,45 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 import numpy as np
 
 
-def computed_infection_time_per_district(sir_object):
+def compute_infection_time_per_district(inf_arr, district_arr):
     """
     Return a dict keyed by district contained in node metadata of the graph `sir.G`. Each value
     is a list of all sorted infection times in the district.
     """
     district_inf_times = defaultdict(list)
     # Iterate over infection times of each node in the simulation
-    for i, t in enumerate(sir_object.inf_occured_at):
-        if (t > 0) and (t < np.inf):
-            node_district = sir_object.G.node[sir_object.idx_to_node[i]]['district']
-            district_inf_times[node_district].append(t)
+    for i, (inf_time, district) in enumerate(zip(inf_arr, district_arr)):
+        if (inf_time > 0) and (inf_time < np.inf):
+            district_inf_times[district].append(inf_time)
     # Format as dict and sort values
     district_inf_times = dict(district_inf_times)
     for k, v in district_inf_times.items():
-        district_inf_times[k] = sorted(v)
+        district_inf_times[k] = np.array(sorted(v))
     return district_inf_times
+
+
+def compute_r0_per_country(inf_time_arr, infector_arr, country_arr):
+    """
+    Compute the basic reproduction number R0 per country for the given data.
+
+    inf_time_arr : array-like (shape: (N,))
+        Infection time of each of the N nodes
+    infector_arr : array-like (shape: (N,))
+        Index of the
+    """
+    # Count of secondary infections: {node_idx: num of infected neighbors}
+    infector_count = Counter(infector_arr)
+    # Indices of infected nodes
+    infected_node_indices = np.where(np.array(inf_time_arr) < np.inf)[0]
+    # Initialize the list of number of secondary infections per country
+    country_count = defaultdict(list)
+    # For each infected node, add its number of secondary case to its country
+    for u_idx in infected_node_indices:
+        u_country = country_arr[u_idx]
+        inf_count = infector_count[u_idx]
+        country_count[u_country].append(inf_count)
+    country_count = dict(country_count)
+    # Compute R0 as the mean number of secondary case for each country
+    countru_r0_dict = {country: np.mean(count) for country, count in country_count.items()}
+    return countru_r0_dict
