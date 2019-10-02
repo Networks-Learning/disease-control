e588c8adadb1816f808f4c6b1134def9278cd17e
trouleau <william.trouleau@epfl.ch>
2019-10-02 14:40:54 +0200

    Update ebola_base_graph with country meta-data + updated graph generation accordingly

--- lib/graph_generation.py
+++ lib/graph_generation.py
@@ -30,9 +30,11 @@ def make_ebola_network(n_nodes, p_in, p_out, seed=None):
         base_graph_data = json.load(f)
     base_graph = nx.readwrite.json_graph.node_link_graph(base_graph_data)
     cluster_names = list(base_graph.nodes())
+    country_names = [d['country'] for n, d in base_graph.nodes(data=True)]
     cluster_sizes = [int(np.ceil(n_nodes * base_graph.node[u]['size']))
                      for u in cluster_names]
-    node_names = np.repeat(cluster_names, cluster_sizes)
+    nodes_district_name = np.repeat(cluster_names, cluster_sizes)
+    nodes_country_name = np.repeat(country_names, cluster_sizes)
     n_nodes = sum(cluster_sizes)
     # Build the intra/inter cluster probability matrix
     base_adj = nx.adjacency_matrix(base_graph).toarray().astype(float)
@@ -41,18 +43,12 @@ def make_ebola_network(n_nodes, p_in, p_out, seed=None):
     # Generate stoch block model graph
     graph = nx.generators.stochastic_block_model(cluster_sizes, base_adj, seed=seed)
     # Assign district attribute to each node
-    for u, district in zip(graph.nodes(), node_names):
+    for u, district, country in zip(graph.nodes(), nodes_district_name, nodes_country_name):
         graph.node[u]['district'] = district
+        graph.node[u]['country'] = country
     # Sanity check for name assignment of each cluster
     num_unique_block_district = len(set([(node_data['block'], node_data['district']) for u, node_data in graph.nodes(data=True)]))
     assert num_unique_block_district == len(cluster_names)
     # Extract the giant component
     graph = max(nx.connected_component_subgraphs(graph), key=len)
-    return graph
-
-
-def load_ebola_network():
-    with open(EBOLA_SCALED_GRAPH_FILE, 'r') as f:
-        graph_data = json.load(f)
-        graph = nx.readwrite.json_graph.node_link_graph(graph_data)
     return graph
